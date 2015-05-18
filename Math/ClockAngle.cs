122285e283aba881d9eb1090db4b419acbfdcc3a
Satish Kumaar Ponnusamy <satishkumaar.p@gmail.com>
2015-05-17 18:24:49 -0700

    Check whether a given linked list is palindrome or not using recursion Clock angle problem

---
+++ Math/ClockAngle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TryingOut.Math
+{
+    public class ClockAngle
+    {
+        private const int OneMinuteAngleForMinuteHand = 360 / 60;
+        private const int OneHourAngleForHourHand = 360/12;
+        private const decimal OneMinuteAngleForHourHand = OneHourAngleForHourHand/60m;
+        private const int MaxAngle = 180;
+        private const int FullAngle = 360;
+
+        public decimal ForHour(int hour)
+        {
+            var angle = hour * OneHourAngleForHourHand;
+            return CorrectAngleFor180(angle);
+        }
+
+        private static decimal CorrectAngleFor180(decimal angle)
+        {
+            return angle > MaxAngle ? FullAngle - angle : angle;
+        }
+
+        public decimal ForMinute(int minute)
+        {
+            var angle = minute * OneMinuteAngleForMinuteHand;
+            return CorrectAngleFor180(angle);
+        }
+
+        public decimal GetAngleBetweenHourAndMinuteHand(int hour, int minute)
+        {
+            var minuteAngle = minute * OneMinuteAngleForMinuteHand;
+            var hourAngle = hour * OneHourAngleForHourHand;
+            var angleMovedByHourHandForGivenMinutes = minute*OneMinuteAngleForHourHand;
+            var totalHourAngle = hourAngle + angleMovedByHourHandForGivenMinutes;
+
+            var angle = System.Math.Abs(totalHourAngle - minuteAngle);
+            return CorrectAngleFor180(angle);
+        }
+
+        public List<Time> GetTimesForAngle(int angle)
+        {
+            var times = new List<Time>();
+            foreach (var hour in Enumerable.Range(0, 12))
+            {
+                foreach (var minute in Enumerable.Range(0, 60))
+                {
+                    //Console.WriteLine("Hour: {0} Minute: {1} Angle: {2}", hour, minute, GetAngleBetweenHourAndMinuteHand(hour, minute));
+                    if (angle == GetAngleBetweenHourAndMinuteHand(hour, minute))
+                    {
+                        times.Add(new Time{ Hour = hour, Minute = minute});
+                    }
+                }
+            }
+            return times;
+        }
+    }
+
+    public class Time
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+    }
+}
---
+++ TryingOut.Tests/LinkedList/PalindromeTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TryingOut.LinkedList;
+
+namespace TryingOut.Tests.LinkedList
+{
+    class PalindromeTests
+    {
+        private readonly Palindrome _palindrome = new Palindrome();
+
+        [Test]
+        public void ShouldThrowArgumentNullException()
+        {
+            Action a = () => _palindrome.Check(null);
+            a.ShouldThrow<ArgumentNullException>();
+        }
+
+        private object[] _testCases = new[]
+        {
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b'}),
+                false
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'b'}),
+                false
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'b', 'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'd', 'a'}),
+                false
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'c', 'b', 'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'c', 'd', 'a'}),
+                false
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'c', 'd', 'e', 'd', 'c', 'b', 'a'}),
+                true
+            },
+            new object[]
+            {
+                new LinkedList<char>(new [] {'a', 'b', 'c', 'd', 'e', 'e', 'c', 'b', 'a'}),
+                false
+            }
+        };
+
+        [TestCaseSource("_testCases")]
+        public void ShouldCheck(LinkedList<char> list, bool isPalindrome)
+        {
+            Print(list);
+
+            _palindrome.Check(list.First).Should().Be(isPalindrome);
+        }
+
+        private void Print(LinkedList<char> list)
+        {
+            var node = list.First;
+            while (node != null)
+            {
+                Console.Write(node.Value);
+                node = node.Next;
+            }
+        }
+    }
+}
