bba5e8ccd2b9c1365bbbccbfe72f0d0bcfb32b2a
Satish Kumaar Ponnusamy <satishkumaar.p@gmail.com>
2015-05-18 18:03:09 -0700

    Finding sub array with largest sum from an array

---
+++ DP/SubArrayWithLargestSum.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace TryingOut.DP
+{
+    public class SubArrayWithLargestSum
+    {
+        public List<int> Find(List<int> array)
+        {
+            if (array == null || array.Count == 0)
+            {
+                return null;
+            }
+
+            var maxSum = int.MinValue;
+            var sum = 0;
+            var subArray = new List<int>();
+            var tempSubArray = new List<int>();
+            bool checkMax = false;
+
+            for (var i = 0; i < array.Count; i++)
+            {
+                if (sum < sum + array[i])
+                {
+                    sum += array[i];
+                    tempSubArray.Add(array[i]);
+                }
+                else
+                {
+                    sum = 0;
+                    tempSubArray.Clear();
+                }
+
+                if (maxSum < sum)
+                {
+                    maxSum = sum;
+                    subArray = tempSubArray;
+                }
+            }
+
+            return subArray;
+        }
+    }
+}
---
+++ TryingOut.Tests/DP/SubArrayWithLargestSumTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using TryingOut.DP;
+
+namespace TryingOut.Tests.DP
+{
+    class SubArrayWithLargestSumTests
+    {
+        private object[] _testCases = new[]
+        {
+            new object[]
+            {
+                new List<int>{1, 2, 3, 0, 7, 2},
+                new List<int>{7, 2}
+            },
+            new object[]
+            {
+                new List<int>{10, 0, 7, 4, 0, 11},
+                new List<int>{11}
+            }
+        };
+
+        private readonly SubArrayWithLargestSum _subArrayWithLargestSum = new SubArrayWithLargestSum();
+
+        [TestCaseSource("_testCases")]
+        public void ShouldReturnSubArrayWithLargestSum(List<int> array, List<int> subArray)
+        {
+            Console.WriteLine("Array: ");
+            array.ForEach(x => Console.Write(x + ","));
+
+            var result = _subArrayWithLargestSum.Find(array);
+
+            Console.WriteLine("\nSubArray: ");
+            result.ForEach(x => Console.Write(x + ","));
+            
+            result.ShouldBeEquivalentTo(subArray);
+        }
+    }
+}
