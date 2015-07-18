2be18c3d85923011c48766c6dcb898a44c2ff48a
unknown <Dev@POR11000HHH8TY1.uss.net>
2015-07-18 11:17:30 -0700

    Constructing binary tree given inorder and either preorder or postorder

--- Trees/Traversal.cs
+++ Trees/Traversal.cs
@@ -1,12 +1,95 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TryingOut.Trees
 {
     public class Traversal
     {
-        public static Node ConstructTreeFrom(List<int> inOrder, List<int> postOrder)
+        public static Node ConstructBinaryTreeFromPreAndInorder(List<int> preOrder, List<int> inOrder)
         {
-            return null;
+            if (inOrder == null || preOrder == null || inOrder.Count == 0 || preOrder.Count == 0 || inOrder.Count != preOrder.Count)
+            {
+                throw new ArgumentException("Null or empty or number of nodes not matching");
+            }
+
+            var mapInOrderDataToIndex = new Dictionary<int, int>();
+            for (var i = 0; i < inOrder.Count; i++)
+            {
+                mapInOrderDataToIndex.Add(inOrder[i], i);
+            }
+
+            //return ConstructTreeFromPreAndInorder(preOrder, 0, preOrder.Count - 1, inOrder, 0, inOrder.Count - 1, mapInOrderDataToIndex);
+            return ConstructBinaryTreeFromPreAndInorder(preOrder, 0, 0, inOrder.Count - 1, mapInOrderDataToIndex);
+        }
+
+        //private static Node ConstructTreeFromPreAndInorder(List<int> preOrder, int preOrderStartIndex, int preOrderEndIndex, List<int> inOrder, int inOrderStartIndex, int inOrderEndIndex, Dictionary<int, int> mapOfInOrderDataToIndex)
+        //{
+        //    if (inOrderStartIndex > inOrderEndIndex)
+        //    {
+        //        return null;
+        //    }
+
+        //    var node = new Node(preOrder[preOrderStartIndex++]);
+
+        //    var inOrderRootIndex = mapOfInOrderDataToIndex[node.Data];
+
+        //    node.Left = ConstructTreeFromPreAndInorder(preOrder, preOrderStartIndex, preOrderStartIndex + inOrderRootIndex - 1 - inOrderStartIndex,
+        //                                               inOrder, inOrderStartIndex, inOrderRootIndex - 1, mapOfInOrderDataToIndex);
+
+        //    node.Right = ConstructTreeFromPreAndInorder(preOrder, preOrderStartIndex + inOrderRootIndex - 1 - inOrderStartIndex + 1, preOrderEndIndex, 
+        //                                                inOrder, inOrderRootIndex + 1, inOrderEndIndex, mapOfInOrderDataToIndex);
+
+        //    return node;
+        //}
+
+        private static Node ConstructBinaryTreeFromPreAndInorder(List<int> preOrder, int preOrderStartIndex, int inOrderStartIndex, int inOrderEndIndex, Dictionary<int, int> mapOfInOrderDataToIndex)
+        {
+            if (inOrderStartIndex > inOrderEndIndex)
+            {
+                return null;
+            }
+
+            var node = new Node(preOrder[preOrderStartIndex++]);
+
+            var inOrderRootIndex = mapOfInOrderDataToIndex[node.Data];
+
+            node.Left = ConstructBinaryTreeFromPreAndInorder(preOrder, preOrderStartIndex, inOrderStartIndex, inOrderRootIndex - 1, mapOfInOrderDataToIndex);
+
+            node.Right = ConstructBinaryTreeFromPreAndInorder(preOrder, preOrderStartIndex + inOrderRootIndex - inOrderStartIndex, inOrderRootIndex + 1, inOrderEndIndex, mapOfInOrderDataToIndex);
+
+            return node;
+        }
+
+        public static Node ConstructBinaryTreeFromInOrderAndPostOrder(List<int> inOrder, List<int> postOrder)
+        {
+            if (inOrder == null || postOrder == null || inOrder.Count == 0 || postOrder.Count == 0 || inOrder.Count != postOrder.Count)
+            {
+                throw new ArgumentException("Null or empty or number of nodes not matching");
+            }
+
+            var mapInOrderDataToIndex = new Dictionary<int, int>();
+            for (var i = 0; i < inOrder.Count; i++)
+            {
+                mapInOrderDataToIndex.Add(inOrder[i], i);
+            }
+
+            return ConstructBinaryTreeFromInorderAndPostOrder(postOrder, postOrder.Count - 1, 0, inOrder.Count - 1, mapInOrderDataToIndex);
+        }
+
+        private static Node ConstructBinaryTreeFromInorderAndPostOrder(List<int> postOrder, int postOrderEndIndex, int inOrderStartIndex, int inOrderEndIndex, Dictionary<int, int> mapInOrderDataToIndex)
+        {
+            if (inOrderStartIndex > inOrderEndIndex)
+            {
+                return null;
+            }
+
+            var node = new Node(postOrder[postOrderEndIndex--]);
+            var inOrderRootIndex = mapInOrderDataToIndex[node.Data];
+
+            node.Left = ConstructBinaryTreeFromInorderAndPostOrder(postOrder, postOrderEndIndex - (inOrderEndIndex - inOrderRootIndex), inOrderStartIndex, inOrderRootIndex - 1, mapInOrderDataToIndex);
+            node.Right = ConstructBinaryTreeFromInorderAndPostOrder(postOrder, postOrderEndIndex, inOrderRootIndex + 1, inOrderEndIndex, mapInOrderDataToIndex);
+
+            return node;
         }
 
         public static List<int> PreOrderWithoutRecursion(Node node)
--- TryingOut.Tests/Trees/TraversalTests.cs
+++ TryingOut.Tests/Trees/TraversalTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 using TryingOut.Trees;
@@ -75,6 +76,128 @@ namespace TryingOut.Tests.Trees
             Traversal.PreOrderWithoutRecursion(root).Should().ContainInOrder(preOrder);
             Traversal.InOrderWithoutRecursion(root).Should().ContainInOrder(inOrder);
             Traversal.PostOrderWithoutRecursion(root).Should().ContainInOrder(postOrder);
+        }
+
+        private readonly object[] _testData1 =
+        {
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7))),
+                new List<int> {1, 2, 4, 5, 3, 6, 7},
+                new List<int> {4, 2, 5, 1, 6, 3, 7}
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), null), null),
+                new List<int> {1, 2, 4},
+                new List<int> {4, 2, 1},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), new Node(5)), null),
+                new List<int> {1, 2, 4, 5},
+                new List<int> {4, 2, 5, 1},
+            },
+            new object[]
+            {
+                new Node(1, null, new Node(3, null, new Node(7))),
+                new List<int> {1, 3, 7},
+                new List<int> {1, 3, 7},
+            },
+            new object[]
+            {
+                new Node(1, null, new Node(3, new Node(6), new Node(7))),
+                new List<int> {1, 3, 6, 7},
+                new List<int> {1, 6, 3, 7},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), null), new Node(3, null, new Node(7))),
+                new List<int> {1, 2, 4, 3, 7},
+                new List<int> {4, 2, 1, 3, 7},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, null, new Node(5)), new Node(3, new Node(6), null)),
+                new List<int> {1, 2, 5, 3, 6},
+                new List<int> {2, 5, 1, 6, 3},
+            }
+        };
+
+        [TestCaseSource("_testData1")]
+        public void ShouldConstructGivenTreeFromPreOrderAndInorderTraversal(Node root, List<int> preOrder, List<int> inOrder)
+        {
+            var node = Traversal.ConstructBinaryTreeFromPreAndInorder(preOrder, inOrder);
+            ThenVerifyTreeMatches(root, node).Should().BeTrue();
+        }
+
+        private readonly object[] _testData2 =
+        {
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7))),
+                new List<int> {4, 2, 5, 1, 6, 3, 7},
+                new List<int> {4, 5, 2, 6, 7, 3, 1}
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), null), null),
+                new List<int> {4, 2, 1},
+                new List<int> {4, 2, 1},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), new Node(5)), null),
+                new List<int> {4, 2, 5, 1},
+                new List<int> {4, 5, 2, 1},
+            },
+            new object[]
+            {
+                new Node(1, null, new Node(3, null, new Node(7))),
+                new List<int> {1, 3, 7},
+                new List<int> {7, 3, 1},
+            },
+            new object[]
+            {
+                new Node(1, null, new Node(3, new Node(6), new Node(7))),
+                new List<int> {1, 6, 3, 7},
+                new List<int> {6, 7, 3, 1},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, new Node(4), null), new Node(3, null, new Node(7))),
+                new List<int> {4, 2, 1, 3, 7},
+                new List<int> {4, 2, 7, 3, 1},
+            },
+            new object[]
+            {
+                new Node(1, new Node(2, null, new Node(5)), new Node(3, new Node(6), null)),
+                new List<int> {2, 5, 1, 6, 3},
+                new List<int> {5, 2, 6, 3, 1},
+            }
+        };
+
+        [TestCaseSource("_testData2")]
+        public void ShouldConstructGivenTreeFromInOrderAndPostorderTraversal(Node root, List<int> inOrder, List<int> postOrder)
+        {
+            var node = Traversal.ConstructBinaryTreeFromInOrderAndPostOrder(inOrder, postOrder);
+            ThenVerifyTreeMatches(root, node).Should().BeTrue();
+        }
+
+        private bool ThenVerifyTreeMatches(Node node1, Node node2)
+        {
+            if (node1 == null && node2 == null)
+            {
+                return true;
+            }
+
+            if (node1 != null && node2 != null && node1.Data == node2.Data)
+            {
+                Console.WriteLine("A" + node1.Data + " E "+ node2.Data);
+                return ThenVerifyTreeMatches(node1.Left, node2.Left) && ThenVerifyTreeMatches(node1.Right, node2.Right);
+            }
+
+            return false;
         }
     }
 }
